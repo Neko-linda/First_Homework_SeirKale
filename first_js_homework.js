6fb643fa4e3861e249a510787cf14f9ed49c5402
User Name <emelinda.gutierrez04@gmail.com>
2023-05-04 22:11:27 -0400

    First homework complete

---
+++ first_js_homework.js
@@ -0,0 +1,134 @@
+// 1. The difference between interpolation and
+// concatentation is . . .
+
+
+//What does the acronym DRY stand for? Why should we pay attention to it? What programming tools have we learned to write DRY code?
+
+//DRY stands for Don't Repeat Yourself. DRY is important for making your code readable and easier to edit for both you and others.
+
+//What is the difference between const and let and var? Please limit your answer to no more than three short sentences. Don't be afraid to research using google and other resources
+
+//Const is used for a variable that will not change under any circumstance and no other variable can go by the same name. Let is used to reassign values. Var is used for the same but also for hoisting, while let doesnt involve hoiting. 
+
+//--------------
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kelvin';
+const f = false;
+
+// -------
+
+a == b;
+
+c == d;
+
+// 'Name' = 'Name';
+
+a + b != c;
+
+a * a != d;
+
+e != 'Kelvin';
+
+48 == '48';
+
+f == e;
+
+let g = 110;
+
+console.log("hello world") // testing CL
+console.log(g);
+
+//Did you use const, let or var? Why did you choose the one you chose?
+
+//I chose let since we would be changing the variable of g. Const is for variables that do not change, and  since we are not "hoisting"
+
+//What happens if you don't use const , let or var? Do you get an error? If so what does it say?
+
+//The code still runs. There is no error. having g = something still counts as it having a value. 
+
+//what happens if you write 10 = g?
+
+// 10 = g;
+
+// console.log(g)
+
+//There is a syntax error. "SyntaxError: Invalid left-hand side in assignment"
+
+//-----
+
+//Is the code below an infinite loop? Why or why not?
+
+//while (true) {
+// 	console.log('Do not run this loop');
+// }
+
+//yes. The condition isnt set to false which means it will always evaluate to true, and as long as the condition is true the loop will repeat, indefinitely.
+
+//is this loop an infinite loop? Why or why not?
+
+//const runProgram = true;
+
+// while (runProgram) {
+// 	console.log('Do not run this loop');
+// 	runProgram = false;
+// }
+
+//no. This loop is not infinite because there is a condition set to false, meaning if that condition is met, the loop will stop. the expected output is it will be a true statement, but after the string is console logged, it will become flase so the loop will stop.
+
+//------
+
+let letters = "A";
+//sets a value to the variable "letters"
+let i = 0;
+//sets value of "i" to 0
+
+while (i < 20) {
+    //begins a while loop thats runs as long as i is less than 20.
+	letters += "A";
+    // addition operator adds 1 "A" until it reaches 20.
+	i++;    
+    //Incirments by 1 
+}
+
+console.log(letters);
+
+//the console will print 20 A's.
+
+//the code was what i expected.
+
+//------
+
+//A for loop performs the same operation as a while loop. But what are the key differences, if any? What are the similarities?
+
+//They do the same thing apart from the fact that while loops cannot initalize variables.
+
+
+//What are the three components of the control panel? Each component is separated by a semicolon ;
+
+for (let i = 0; 
+    // The first part of the control panel is: for initializies the loop while "Let" sets the value for i to 0.
+    i < 100; 
+    // The second part of the control panel is: as long as i is less than 100 the loop will continue
+    i++) {
+	console.log('Without you, today\'s emotions are the scurf of yesterday\'s');
+    //The third part of the control panel is: logs the string 100 times in the console.
+}
+
+//Write a for loop that will console.log the numbers 0 to 999.
+
+for (let i = 0; i < 999; i++) {
+    console.log(i)
+}
+
+// ------
+
+const StarWars = ["Han", "C3PO", "R2D2", "Luke", "Leia", "Anakin"];
+
+for (let i = 0; i < StarWars.length; i++) {
+    console.log(StarWars[i]);
+}
+
+//------
---
+++ terminal_homework.bash
@@ -0,0 +1,247 @@
+ #Copy this text into your `.bash` homework file. Copy and paste the code you successfully used into terminal below each prompt
+
+# Open the **Terminal app**
+
+# Create a new directory on your desktop called `galaxy_far_far_away`and enter it
+
+#mkdir galaxy_far_far_away
+
+# Create a directory called `death_star`
+
+#cd galaxy_far_far_away 
+#mkdir death_star
+
+# and make the following files inside of it:
+# darth_vader.txt
+# princess_leia.txt
+# storm_trooper.txt
+
+
+#touch darth_vader.txt
+#  death_star touch princess_leia.txt storm
+#_trooper.txt
+#➜  death_star ls
+#darth_vader.txt   storm_trooper.txt
+#princess_leia.txt
+
+# In `galaxy_far_far_away`, make a directory named `tatooine`
+
+# cd ..
+# ➜  galaxy_far_far_away mkdir tatooine
+# ➜  galaxy_far_far_away 
+
+# and create the following files in it:
+# luke.txt
+# ben_kenobi.txt
+
+# tatooine touch luke.txt ben_kenobi.txt
+# ➜  tatooine 
+
+# Inside of `tatooine` make a directory called `millenium_falcon`
+# and in it create:
+# han_solo.txt
+# chewbaca.txt
+
+#  cd tatooine 
+# ➜  tatooine touch luke.txt ben_kenobi.txt
+# ➜  tatooine mkdir millenium_falcon
+# ➜  tatooine cd millenium_falcon 
+# ➜  millenium_falcon touch han_solo.txt chew
+# bacca.txt
+
+# Rename `ben_kenobi.txt` to `obi_wan.txt
+
+# cd ..
+# ➜  tatooine ls
+# ben_kenobi.txt   millenium_falcon
+# luke.txt
+# ➜  tatooine mv ben_kenobi.txt obi_wan.txt
+# ➜  tatooine ls
+# luke.txt         obi_wan.txt
+
+# Copy `storm_trooper.txt` from `death_star` to `tatooine`
+
+# galaxy_far_far_away cp death_star/storm_
+# trooper.txt tatooine
+# ➜  galaxy_far_far_away ls
+# death_star tatooine
+# ➜  galaxy_far_far_away cd tatooine 
+# ➜  tatooine ls
+# luke.txt          obi_wan.txt
+# millenium_falcon  storm_trooper.txt
+
+# Move `luke.txt` and `obi_wan.txt` to the `millenium_falcon`
+
+# tatooine mv luke.txt obi_wan.txt milleni
+# um_falcon 
+# ➜  tatooine ls
+# millenium_falcon  storm_trooper.txt
+# ➜  tatooine cd millenium_falcon 
+# ➜  millenium_falcon ls
+# chewbacca.txt luke.txt
+# han_solo.txt  obi_wan.txt
+
+# Move `millenium_falcon` out of `tatooine` and into `galaxy_far_far_away`
+
+# Move `millenium_falcon` into `death_star`
+
+# tatooine mv millenium_falcon ../ 
+# ➜  galaxy_far_far_away mv millenium_falcon death_star 
+# ➜  galaxy_far_far_away ls
+# death_star tatooine
+# ➜  galaxy_far_far_away cd death_star 
+# ➜  death_star ls
+# darth_vader.txt   princess_leia.txt
+# millenium_falcon  storm_trooper.txt
+
+# Move `princess_leia.txt` into the `millenium_falcon`
+
+# ➜  death_star mv princess_leia.txt millenium_falcon
+# ➜  death_star ls
+# darth_vader.txt   storm_trooper.txt
+# millenium_falcon
+# ➜  death_star millenium_falcon 
+# ➜  millenium_falcon ls
+# chewbacca.txt     obi_wan.txt
+# han_solo.txt      princess_leia.txt
+# luke.txt
+
+# Delete `obi_wan.txt`
+
+# rm obi_wan.txt 
+
+# In `galaxy_far_far_away`, make a directory called `yavin_4`
+
+# mkdir yavin_4
+
+# Move the `millenium_falcon` out of the `death_star` and into `yavin_4`
+
+# cd death_star 
+# ➜  death_star millenium_falcon ../
+# zsh: command not found: millenium_falcon
+# ➜  death_star mv millenium_falcon ../
+# ➜  death_star ls
+# darth_vader.txt   storm_trooper.txt
+# ➜  death_star cd ..
+# ➜  galaxy_far_far_away ls
+# death_star       tatooine
+# millenium_falcon yavin_4
+
+# Make a directory in `yavin_4` called `x_wing`
+
+#mkdir x_wing
+
+# Move `princess_leia.txt` to `yavin_4` and `luke.txt` to `x_wing`
+
+# cd millenium_falcon
+# ➜  millenium_falcon ls
+# chewbacca.txt     luke.txt
+# han_solo.txt      princess_leia.txt
+# ➜  millenium_falcon mv princess_leia.txt ..
+# /
+# ➜  millenium_falcon cd ..
+# ➜  galaxy_far_far_away ls
+# death_star        tatooine
+# millenium_falcon  yavin_4
+# princess_leia.txt
+# ➜  galaxy_far_far_away mv princess_leia.txt yavin_4 
+# ➜  galaxy_far_far_away ls
+# death_star       tatooine
+# millenium_falcon yavin_4
+# ➜  galaxy_far_far_away cd yavin_4 
+# ➜  yavin_4 ls
+# princess_leia.txt x_wing
+
+
+#  millenium_falcon mv luke.txt ../
+# ➜  millenium_falcon cd ..
+# ➜  galaxy_far_far_away mv luke.txt yavin_4
+# ➜  galaxy_far_far_away cd yavin_4 
+# ➜  yavin_4 mv luke.txt x_wing 
+# ➜  yavin_4 cd x_wing 
+# ➜  x_wing ls
+# luke.txt
+
+# Move the `millenium_falcon` and `x_wing` out of `yavin_4` and into `galaxy_far_far_away`
+
+# In `death_star`, create directories for `tie_fighter_1`, `tie_fighter_2` and `tie_fighter_3`
+
+
+# ➜  death_star mkdir tie_fighter_1 tie_fight
+# er_2 tie_fighter_3
+# ➜  death_star ls
+# darth_vader.txt   tie_fighter_2
+# storm_trooper.txt tie_fighter_3
+# tie_fighter_1
+
+# Move `darth_vader.txt` into `tie_fighter_1`
+
+# ➜  death_star mv darth_vader.txt tie_fighter_1
+
+# Make a copy of `storm_trooper.txt` in both `tie_fighter_2` and `tie_fighter_3`
+
+# ➜  death_star cp storm_trooper.txt tie_figh
+# ter_2
+# ➜  death_star cp storm_trooper.txt tie_figh
+# ter_3
+# ➜  death_star ls
+# storm_trooper.txt tie_fighter_2
+# tie_fighter_1     tie_fighter_3
+# ➜  death_star cd tie_fighter_2
+# ➜  tie_fighter_2 ls
+# storm_trooper.txt
+# ➜  tie_fighter_2 
+
+# Move all of the `tie_fighters` out of the `death_star` and into `galaxy_far_far_away`
+
+
+#  death_star mv tie_fighter_1 tie_fighter_
+# 2 tie_fighter_3 ../
+# ➜  death_star ls
+# storm_trooper.txt
+# ➜  death_star cd ..
+# ➜  galaxy_far_far_away ls
+# death_star       tie_fighter_2
+# millenium_falcon tie_fighter_3
+# tatooine         x_wing
+# tie_fighter_1    yavin_4
+
+# Be careful with this command - cannot undo!
+
+# Make sure you delete the right thing, or you could accidentally delete the contents of your computer (it has happened).
+
+# This command will typically not ask you if you really want to delete. It will just delete so doublecheck you are deleting exactly what you mean to delete
+
+# Remove `tie_fighters` 2 and 3.
+
+# ➜  galaxy_far_far_away rm -r tie_fighter_2 
+# tie_fighter_3
+# ➜  galaxy_far_far_away ls
+# death_star       tie_fighter_1
+# millenium_falcon x_wing
+# tatooine         yavin_4
+
+# Touch a file in "**x_wing**" called "**the_force.txt**".
+
+#cd x_wing 
+# ➜  x_wing touch the_force.txt
+
+# Destroy the "**death_star**" and anyone inside of it.
+
+# galaxy_far_far_away rm -r death_star 
+# ➜  galaxy_far_far_away ls
+# millenium_falcon x_wing
+# tatooine         yavin_4
+# tie_fighter_1
+
+# Return "**x_wing**" and the "**millenium_falcon**" to "**yavin_4**".
+
+# ➜  galaxy_far_far_away mv x_wing yavin_4 
+# ➜  galaxy_far_far_away ls
+# tatooine      tie_fighter_1 yavin_4
+# ➜  galaxy_far_far_away cd yavin_4 
+# ➜  yavin_4 ls
+# millenium_falcon  x_wing
+# princess_leia.txt
+
+# Celebrate!
